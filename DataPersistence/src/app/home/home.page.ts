1c3acde5445de317da602c3be009fa1bab58cec8
unknown <senikdima12@gmail.com>
2025-04-02 10:59:14 +0000

    commit

---
+++ DataPersistence/src/app/home/home.page.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonBackButton } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton,RouterLink],
+})
+export class HomePage {
+  myStatus:string = "";
+  constructor(private storage:Storage) {}
+  
+  async ionViewWillEnter(){
+    await this.storage.create();
+    this.myStatus = await this.storage.get('status');
+  }
+
+}
---
+++ DataPersistence/src/app/status/status.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButtons, IonLabel, IonItem, IonList, IonRadio, IonRadioGroup, IonButton } from '@ionic/angular/standalone';
+import { Storage } from '@ionic/storage-angular';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-status',
+  templateUrl: './status.page.html',
+  styleUrls: ['./status.page.scss'],
+  standalone: true,
+  imports: [IonButton, IonRadioGroup, IonRadio, IonList, IonItem, IonLabel, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonButtons,IonBackButton]
+})
+export class StatusPage implements OnInit {
+
+  constructor(private storage:Storage, private router:Router) {
+   }
+  
+  status:string = "";
+
+  ngOnInit() {
+  }
+
+  async onButtonClick(){
+    await this.storage.create();
+    await this.storage.set("status",this.status);
+    this.router.navigate(['/home']);
+  }
+
+}
